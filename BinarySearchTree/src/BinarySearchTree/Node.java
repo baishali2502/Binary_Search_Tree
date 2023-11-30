1d85b643a9865682a80cccfcd7d75adbf510f5a5
baishali2502 <baishali2502@gmail.com>
2023-11-30 07:19:33 +0530

    Defining node of a BST

---
+++ BinarySearchTree/src/BinarySearchTree/Node.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+
+//Defining node of a Binary-Search Tree
+public class Node {
+	int data;
+	Node left;
+	Node right;
+	
+	Node(int data)
+	{
+		this.data = data;
+		left=null;
+		right=null;
+	}
+
+}
